c297c3df7234205aab784b0e6413e38354ad0642
Scott McConkey <scottmcconkey7@gmail.com>
2017-11-05 09:54:17 -0700

    Fix conversion script for inventory

--- Database/Migration/convert_inventory_items.sql
+++ Database/Migration/convert_inventory_items.sql
@@ -8,41 +8,39 @@ INSERT INTO public.inventory_items(
 	stockacct, 
 	cogacct, 
 	sellunit, 
-	stocktakenewqty, 
+	--stocktakenewqty, 
 	flags,  
 	sohcount, 
 	buyprice, 
 	buyunit, 
-	buyprice, 
-	category, 
 	qtybrksellprice, 
 	sellcurrency, 
 	buycurrency, 
 	costprice
 	)
-	VALUES (
+	
+	select 
 	"Code", --code 
-	"Description", --description, 
-	select cast("StockValue" as double precision), --stockvalue
-	select cast("SellPrice" as double precision), --sellprice
+	coalesce("Description", 'Conversion - No Description Provided'), --description, 
+	cast("StockValue" as double precision), --stockvalue
+	cast("SellPrice" as double precision), --sellprice
 	'CONVERSION: \n' || 
 		'Original Comment: ' || "Comment" || '\n' ||
 		'Original SellUnit: ' || "SellUnit" || '\n' ||
 		'Original BuyUnit: ' || "BuyUnit" || '\n' ||
-		'Original Buy Currency: ' || "BuyPriceCurrency" || '\n', --comment
-	select cast("SalesAcct" as integer), --salesacct
-	select cast("StockAcct" as integer), --stockacct
-	select cast("COGAcct" as integer), --cogacct
+		'Original Buy Currency: ' || "BuyPriceCurrency" || '\n'
+		'Original Category: ' || "Category1", --comment
+	cast("SalesAcct" as integer), --salesacct
+	cast("StockAcct" as integer), --stockacct
+	cast("COGAcct" as integer), --cogacct
 	"SellUnit", --sellunit
-	select cast("StockTakeNewQty" as integer), --stocktakenewqty
-	select cast("Flags" as integer), --flags
-	select cast("SOH/Count" as integer), --sohcount
-	select cast("BuyPrice" as double precision), --buyprice
+	--cast(trim("StockTakeNewQty") as double precision), --stocktakenewqty
+	cast("Flags" as integer), --flags
+	cast("SOH/Count" as double precision), --sohcount
+	cast("BuyPrice" as double precision), --buyprice
 	"BuyUnit", --buyunit
-	select cast("BuyPrice" as double precision), --buyprice
-	"Category1", --category
-	select cast("QtyBrkSellPriceA1" as double precision), --qtybrksellprice
+	cast("QtyBrkSellPriceA1" as double precision), --qtybrksellprice
 	"BuyPriceCurrency", --sellcurrency
 	"BuyPriceCurrency", --buycurrency
-	select cast("CostPrice" as double precision), --costprice
-	);
+	cast("CostPrice" as double precision) --costprice
+	from public.migrate_inventory;
