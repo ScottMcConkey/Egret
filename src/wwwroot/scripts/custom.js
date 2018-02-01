0126a5870943cc1194075294f2572fe08a96d313
Scott McConkey <scottmcconkey7@gmail.com>
2018-02-01 10:19:46 -0700

    Isolate custom jquery

---
+++ src/wwwroot/scripts/custom.js
@@ -0,0 +1,48 @@
+﻿function SetContentHeight() {
+    $("div#main").css("min-height", $("div.leftnav").height() + "px");
+}
+
+$(document).ready(function () {
+
+
+    $("div#main-top").css("height", $("div.leftnav ul li").height() + 1 + "px");
+    SetContentHeight();
+    $("table.results tr:even").addClass("shaded");
+    $("#main-bottom").css("min-height", $(document).height() - 175 + "px");
+
+
+    // Manage Validation Errors
+    $("input.input-validation-error").closest(".form-group").addClass("has-error");
+    $(".input-validation-error").on("focus", function () {
+        $(this).closest(".form-group").find("input.input-validation-error").val("");
+        $(this).closest(".form-group").removeClass("has-error");
+        $(this).closest(".form-group").find("span.field-validation-error").remove();
+    });
+
+
+    // Manage Tabs
+    $(".tab-header").removeClass("active");
+    $(".tab-header:first").addClass("active");
+    $(".tab-content").css("display", "none");
+    $(".tab-content:first").css("display", "block");
+    $("li.tab-header").on("click", function () {
+        $("li.tab-header").removeClass("active");
+        $(this).addClass("active");
+        var idx = $(".tab-header").index(this);
+        $(".tab-content").css("display", "none");
+        $(".tab-content").eq(idx).css("display", "block");
+        SetContentHeight();
+    });
+
+    // Manage Multi-Row Radio Selects
+    $(".radio[value='False']").removeAttr("checked");
+    $(".radio").on("click", function () {
+
+        if ($(this).attr("checked") == "checked") {
+            $(this).attr("checked", false).attr("value", false);
+        } else {
+            $(".radio").attr("checked", false).attr("value", false);
+            $(this).prop("checked", true).attr("checked", true).attr("value", true);
+        }
+    });
+});
