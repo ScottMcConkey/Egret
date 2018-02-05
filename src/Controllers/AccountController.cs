e5a68eebf7e178bf06b6a8c71eff91f8db8029b1
Scott McConkey <scottmcconkey7@gmail.com>
2018-02-05 14:05:55 -0700

    Update login feature

--- src/Controllers/AccountController.cs
+++ src/Controllers/AccountController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Egret.Models;
 using Egret.ViewModels;
 
 namespace Egret.Controllers
@@ -11,6 +13,16 @@ namespace Egret.Controllers
     [Authorize]
     public class AccountController : Controller
     {
+        private UserManager<AppUser> userManager;
+        private SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> userMgr,
+            SignInManager<AppUser> signinMgr)
+        {
+            userManager = userMgr;
+            signInManager = signinMgr;
+        }
+
         [AllowAnonymous]
         public IActionResult Login(string returnUrl)
         {
@@ -23,6 +35,21 @@ namespace Egret.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginModel details, string returnUrl)
         {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await userManager.FindByEmailAsync(details.Email);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(
+                        user, details.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError(nameof(LoginModel.Email), "Invalid user or password");
+            }
             return View(details);
         }
     }
