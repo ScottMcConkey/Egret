272a495206e76bcabc9bbc795bdb2c733004c363
Scott McConkey <scottmcconkey7@gmail.com>
2018-02-13 10:26:49 -0700

    Purge out old 1.1 references, modify structure to match 2.0 conventions, start clean before tests to (hopefully) avoid nasty 'no tests found' error

--- Egret/Program.cs
+++ Egret/Program.cs
@@ -12,16 +12,6 @@ namespace Egret
     {
         public static void Main(string[] args)
         {
-            //var host = new WebHostBuilder()
-            //    .UseKestrel()
-            //    .UseContentRoot(Directory.GetCurrentDirectory())
-            //    .UseIISIntegration()
-            //    .UseStartup<Startup>()
-            //    .UseApplicationInsights()
-            //    .Build();
-            //
-            //host.Run();
-
             BuildWebHost(args).Run();
         }
 
