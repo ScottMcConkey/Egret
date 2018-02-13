cb5ac20f69d12552730c3433d3666cfbfceec33c
Scott McConkey <scottmcconkey7@gmail.com>
2018-02-13 10:49:24 -0700

    Get tests working. FYI, ProjectReference and Reference in the .csproj are not the same, you just need to realize that these options exist :)

---
+++ Egret.Tests/InventoryTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using Egret.Models;
+
+namespace Egret.Tests
+{
+    public class InventoryTests
+    {
+
+        [Fact]
+        public void ChangeItemName()
+        {
+            // Arrange
+            var i = new InventoryItem { Description = "Test Item", Comment = "This is a test" };
+
+            // Act
+            i.Description = "Modified Test";
+
+            // Assert
+            Assert.Equal("Modified Test", i.Description);
+        }
+    }
+}
