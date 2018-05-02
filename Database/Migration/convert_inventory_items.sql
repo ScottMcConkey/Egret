2697ab009688d9d1b9f5030b38b940fcc1f67899
Scott McConkey <scottmcconkey7@gmail.com>
2018-05-01 21:14:01 -0600

    Update inventory conversion script

--- Database/Migration/convert_inventory_items.sql
+++ Database/Migration/convert_inventory_items.sql
@@ -7,12 +7,10 @@ INSERT INTO public.inventory_items(
 	salesacct, 
 	stockacct, 
 	cogacct, 
-	--sellunit, 
 	stocktakenewqty, 
 	flags,  
 	sohcount, 
 	buyprice, 
-	--buyunit, 
 	qtybrksellprice, 
 	sellcurrency, 
 	buycurrency, 
@@ -23,7 +21,7 @@ INSERT INTO public.inventory_items(
 	
 	select 
 	"Code", --code 
-	coalesce("Description", 'Conversion - No Description Provided'), --description, 
+	coalesce("Description", 'Conversion - No Description Provided'), --description
 	cast("StockValue" as double precision), --stockvalue
 	cast("SellPrice" as double precision), --sellprice
 	'CONVERSION' || E'\n' ||
@@ -31,24 +29,20 @@ INSERT INTO public.inventory_items(
 		'Original SellUnit: ' || coalesce("SellUnit", '') || E'\n' ||
 		'Original BuyUnit: ' || coalesce("BuyUnit", '') || E'\n' ||
 		'Original Buy Currency: ' || coalesce("BuyPriceCurrency", '') || E'\n'
-		'Original Category: ' || coalesce("Category1", ''), 
-		
-		--comment
+		'Original Category: ' || coalesce("Category1", ''), --comment
 	cast("SalesAcct" as integer), --salesacct
 	cast("StockAcct" as integer), --stockacct
 	cast("COGAcct" as integer), --cogacct
-	"SellUnit", --sellunit
 	cast(trim("StockTakeNewQty") as double precision), --stocktakenewqty
 	cast("Flags" as integer), --flags
 	cast("SOH/Count" as double precision), --sohcount
 	cast("BuyPrice" as double precision), --buyprice
-	"BuyUnit", --buyunit
 	cast("QtyBrkSellPriceA1" as double precision), --qtybrksellprice
 	"BuyPriceCurrency", --sellcurrency
 	"BuyPriceCurrency", --buycurrency
 	cast("CostPrice" as double precision), --costprice
-	True,
-	'Money Works Gold'
+	True, --isconversion
+	'Money Works Gold' --conversionsource
 	from public.migrate_inventory;
 	
 commit;
