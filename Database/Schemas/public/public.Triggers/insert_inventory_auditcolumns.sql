2d46be89b414a732320539f4155182cff73b52c3
Scott McConkey <scottmcconkey7@gmail.com>
2018-07-25 09:33:20 -0600

    Remove old database scripts

--- Database/Schemas/public/public.Triggers/insert_inventory_auditcolumns.sql
+++
@@ -1,9 +0,0 @@
--- Trigger: insert_inventory_auditcolumns
-
--- DROP TRIGGER insert_inventory_auditcolumns ON public.inventory_items;
-
-CREATE TRIGGER insert_inventory_auditcolumns
-    BEFORE INSERT
-    ON public.inventory_items
-    FOR EACH ROW
-    EXECUTE PROCEDURE public.initialize_audit_columns();
