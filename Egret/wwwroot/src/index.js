2d2c8146d78235caf01a6f3b291c0b21c23557e6
Scott McConkey <scottmcconkey7@gmail.com>
2019-01-30 14:52:02 -0700

    Change account drop downs, make bootstrap changes and tweak views

--- Egret/wwwroot/src/index.js
+++ Egret/wwwroot/src/index.js
@@ -2,4 +2,9 @@
 import './css/main.css';
 import './css/test.css';
 import './sass/main.scss';
-
+import 'jquery';
+import $ from 'jquery';
+import 'jquery/dist/jquery.min.js';
+import 'jquery/external/sizzle/dist/sizzle.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import './scripts/custom.js';
