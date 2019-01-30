5a7daf5a37d93619f3d7c0376942e78965b40aad
Scott McConkey <scottmcconkey7@gmail.com>
2019-01-30 08:55:28 -0700

    Get fontawesome fonts working with webpack/sass, finally!

--- Egret/webpack.config.js
+++ Egret/webpack.config.js
@@ -23,10 +23,11 @@ module.exports = {
             {
                 test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
                 use: [{
-                    loader: 'file-loader',
+                    loader: 'url-loader',
                     options: {
                         name: '[name].[ext]',
-                        publicPath: '/fonts'
+                        limit: 50,
+                        outputPath: '/dist/fonts'
                     }
                 }]
             }
