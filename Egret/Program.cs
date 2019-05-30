620829fc62c86b48dc161d9d0834e643929656eb
Scott McConkey <scottmcconkey7@gmail.com>
2019-05-30 14:38:19 -0600

    Update package, standalone run experiments

--- Egret/Program.cs
+++ Egret/Program.cs
@@ -13,6 +13,7 @@ namespace Egret
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                //.UseKestrel()
                 .Build();
     }
 }
