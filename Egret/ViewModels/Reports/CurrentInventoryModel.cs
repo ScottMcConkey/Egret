e35aeb2976aa5c1dce8e3d1e38e7098fa0ac75e3
scottmcconkey7@gmail.com <scottmcconkey7@gmail.com>
2019-10-11 19:29:05 -0600

    Add CurrentInventoryModel, becuase somehow that got excluded

---
+++ Egret/ViewModels/Reports/CurrentInventoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Egret.Models;
+
+namespace Egret.ViewModels
+{
+    public class CurrentInventoryModel
+    {
+        public List<CategoryReport> Categories { get; set; }
+    }
+}
